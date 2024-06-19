b6775c37cccf4deb880ed4eb1b62cdf241aeefdb
inception <you@example.com>
2024-06-19 15:59:33 +0900

    [done?] last commit?

--- Makefile
+++ Makefile
@@ -13,9 +13,12 @@ rebuild:
 # Define the clean target
 clean:
 	$(DOCKER_COMPOSE) down
+
+fclean:
+	$(DOCKER_COMPOSE) down
 	docker rmi $$(docker images -q) -f
 	docker volume rm $$(docker volume ls -q)
-	rm -rf srcs/requirements/mariadb/data
-	rm -rf srcs/requirements/wordpress/wordpress
-	mkdir srcs/requirements/mariadb/data
-	mkdir srcs/requirements/wordpress/wordpress
+	rm -rf /home/yeonwkan/data/wordpress
+	rm -rf /home/yeonwkan/data/db
+	mkdir /home/yeonwkan/data/wordpress
+	mkdir /home/yeonwkan/data/db
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-echo "1"
 wp core download --path=/var/www/html
-echo "2"
 cd /var/www/html
-echo "3"
 while [ ! -f /var/www/html/wp-config.php ]; do
 	echo "doing while"
 	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --path=/var/www/html
 	sleep 1
 done
 
-echo "4"
-wp core install --url="https://yeonwkan.42.fr" --title="WP-CLI" --admin_user=yeonwkan --admin_password=yeonwkan --admin_email=mgn1440@gmail.com
-echo "5"
-wp user create author author@example.com --role=author
-echo "6"
+wp core install --url="https://$DOMAIN_NAME" --title="inception" --admin_user=$ADMIN_USER_NAME --admin_password=$ADMIN_USER_PASS --admin_email=mgn1440@gmail.com
+wp user create $AUTHOR_USER_NAME --user_pass=$AUTHOR_USER_PASS author@example.com --role=author
 
-php-fpm81 -F
+php-fpm82 -F
